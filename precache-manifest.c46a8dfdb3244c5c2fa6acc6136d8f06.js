6c0dc9985cde427c96daefdf2b6391d18002fac3
Facundo Petre <facu1p3@gmail.com>
2019-05-04 21:36:49 -0300

    Updates

---
+++ precache-manifest.c46a8dfdb3244c5c2fa6acc6136d8f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3e596895e9afbdb69e2ad5ee400668",
+    "url": "/not-omegle.com/index.html"
+  },
+  {
+    "revision": "a461242cbc24e1b79cc9",
+    "url": "/not-omegle.com/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "41f6021c13b99f63289d",
+    "url": "/not-omegle.com/static/js/2.99cf3e8c.chunk.js"
+  },
+  {
+    "revision": "a461242cbc24e1b79cc9",
+    "url": "/not-omegle.com/static/js/main.e80c11cc.chunk.js"
+  },
+  {
+    "revision": "aaa7e541879f8b8646fe",
+    "url": "/not-omegle.com/static/js/runtime~main.2961cc28.js"
+  }
+]);
